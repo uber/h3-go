dee1fd21266337eff6dfbf9f997ffce3a6d2f3af
Unknown <foo.richard@gmail.com>
2020-08-19 19:41:31 -0400

    Adjusts line test to follow h3 test example.

--- h3_test.go
+++ h3_test.go
@@ -27,8 +27,12 @@ import (
 const eps = 1e-4
 
 // validH3Index resolution 5
-const validH3Index = H3Index(0x850dab63fffffff)
-const pentagonH3Index = H3Index(0x821c07fffffffff)
+const (
+	validH3Index        = H3Index(0x850dab63fffffff)
+	pentagonH3Index     = H3Index(0x821c07fffffffff)
+	validLineStartIndex = H3Index(0x89283082803ffff)
+	validLineEndIndex   = H3Index(0x8929a5653c3ffff)
+)
 
 var (
 	validH3Rings1 = [][]H3Index{
@@ -440,12 +444,13 @@ func TestPolyfill(t *testing.T) {
 }
 
 func TestLine(t *testing.T) {
-	start := validH3Rings1[0][0]
-	end := validH3Rings1[1][0]
-	line := Line(start, end)
-	assert.Equal(t, 2, len(line))
-	assert.Equal(t, start, line[0])
-	assert.Equal(t, end, line[1])
+	t.Parallel()
+	line := Line(validLineStartIndex, validLineEndIndex)
+	assert.Equal(t, validLineStartIndex, line[0])
+	assert.Equal(t, validLineEndIndex, line[len(line)-1])
+	for i := 0; i < len(line)-1; i++ {
+		assert.True(t, AreNeighbors(line[i], line[i+1]))
+	}
 }
 
 func almostEqual(t *testing.T, expected, actual interface{}, msgAndArgs ...interface{}) {
