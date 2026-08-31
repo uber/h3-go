881e7b107a3db6d1b902dbd7652a755532636d0f
gitKashish <125540624+gitKashish@users.noreply.github.com>
2026-09-01 02:37:58 +0530

    chore: use `b.Loop` instead of `range b.N` for benchmark loops

--- bench_test.go
+++ bench_test.go
@@ -27,55 +27,55 @@ var (
 )
 
 func BenchmarkToString(b *testing.B) {
-	for range b.N {
+	for b.Loop() {
 		addr = cell.String()
 	}
 }
 
 func BenchmarkFromString(b *testing.B) {
-	for range b.N {
+	for b.Loop() {
 		cell = Cell(IndexFromString("850dab63fffffff"))
 	}
 }
 
 func BenchmarkLatLng_String(b *testing.B) {
-	for range b.N {
+	for b.Loop() {
 		latlngStr = geo.String()
 	}
 }
 
 func BenchmarkCellToLatLng(b *testing.B) {
-	for range b.N {
+	for b.Loop() {
 		geo, _ = CellToLatLng(cell)
 	}
 }
 
 func BenchmarkLatLngToCell(b *testing.B) {
-	for range b.N {
+	for b.Loop() {
 		cell, _ = LatLngToCell(geo, 15)
 	}
 }
 
 func BenchmarkCellToBoundary(b *testing.B) {
-	for range b.N {
+	for b.Loop() {
 		geoBndry, _ = CellToBoundary(cell)
 	}
 }
 
 func BenchmarkGridDisk(b *testing.B) {
-	for range b.N {
+	for b.Loop() {
 		cells, _ = cell.GridDisk(10)
 	}
 }
 
 func BenchmarkGridRing(b *testing.B) {
-	for range b.N {
+	for b.Loop() {
 		cells, _ = cell.GridRing(10)
 	}
 }
 
 func BenchmarkPolyfill(b *testing.B) {
-	for range b.N {
+	for b.Loop() {
 		cells, _ = PolygonToCells(validGeoPolygonHoles, 13)
 	}
 }
@@ -83,63 +83,63 @@ func BenchmarkPolyfill(b *testing.B) {
 func BenchmarkGridDisksUnsafe(b *testing.B) {
 	cells, _ = PolygonToCells(validGeoPolygonHoles, 12)
 
-	b.ResetTimer()
+	
 
-	for range b.N {
+	for b.Loop() {
 		disks, _ = GridDisksUnsafe(cells, 10)
 	}
 }
 
 func BenchmarkGreatCircleDistanceRads(b *testing.B) {
-	for range b.N {
+	for b.Loop() {
 		distResult = GreatCircleDistanceRads(geo, geo2)
 	}
 }
 
 func BenchmarkGreatCircleDistanceKm(b *testing.B) {
-	for range b.N {
+	for b.Loop() {
 		distResult = GreatCircleDistanceKm(geo, geo2)
 	}
 }
 
 func BenchmarkGreatCircleDistanceM(b *testing.B) {
-	for range b.N {
+	for b.Loop() {
 		distResult = GreatCircleDistanceM(geo, geo2)
 	}
 }
 
 func BenchmarkResolution(b *testing.B) {
-	for range b.N {
+	for b.Loop() {
 		intResult = cell.Resolution()
 	}
 }
 
 func BenchmarkBaseCellNumber(b *testing.B) {
-	for range b.N {
+	for b.Loop() {
 		intResult = cell.BaseCellNumber()
 	}
 }
 
 func BenchmarkIsValid(b *testing.B) {
-	for range b.N {
+	for b.Loop() {
 		boolResult = cell.IsValid()
 	}
 }
 
 func BenchmarkIsPentagon(b *testing.B) {
-	for range b.N {
+	for b.Loop() {
 		boolResult = cell.IsPentagon()
 	}
 }
 
 func BenchmarkIsResClassIII(b *testing.B) {
-	for range b.N {
+	for b.Loop() {
 		boolResult = cell.IsResClassIII()
 	}
 }
 
 func BenchmarkIsValidIndex(b *testing.B) {
-	for range b.N {
+	for b.Loop() {
 		validResult = IsValidIndex(cell)
 	}
 }
